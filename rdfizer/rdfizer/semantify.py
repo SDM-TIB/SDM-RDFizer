f33e30638750f0a902758ac01241b4b2560895ba
samiscoding <samaneh.jozashoori@gmail.com>
2020-08-07 17:54:02 +0200

    Update semantify.py
    tabs fixed!

--- rdfizer/rdfizer/semantify.py
+++ rdfizer/rdfizer/semantify.py
@@ -3308,9 +3308,9 @@ def translate_sql(triples_map):
     for p in proyections:
     	for pr in p:
 	        if pr is not "None":
-                temp_query += "`" + pr + "`, " 
-            else:
-	            temp_query = temp_query[:-2]
+	        	temp_query += "`" + pr + "`, " 
+	        else:
+	        	temp_query = temp_query[:-2]
     temp_query = temp_query[:-2]  
     if triples_map.tablename != "None":
         temp_query = temp_query + " FROM " + triples_map.tablename + ";"
