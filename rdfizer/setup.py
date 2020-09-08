8031a2b5e846f0fd9aaac708f0379566aa1c2e25
Daniel Doña <daniel.dona@xnor.ga>
2020-09-08 12:32:19 +0200

    New setup.py

--- rdfizer/setup.py
+++ rdfizer/setup.py
@@ -1,17 +1,31 @@
 import setuptools
+import argparse
 import time
 
 v_time = str(int(time.time()))
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-k', help="Release type", dest="kind")
+#parser.add_argument('-t', help="Version tag", dest="tag")
+args = parser.parse_args()
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+with open("VERSION", "r") as fh:
+    v = fh.read().replace("\n", "")
+    if args.kind == "rel":
+        vers_taged = v
+    else:
+        vers_taged = v+".dev"+v_time
+
 
 with open("requirements.txt") as r:
     requirements = list(filter(None, r.read().split("\n")[0:]))
 
 setuptools.setup(
     name="rdfizer",
-    version="3.2."+v_time,
+    version=vers_taged,
     author="Maria-Esther Vidal",
     author_email="maria.vidal@tib.eu",
     license="Apache 2.0",
