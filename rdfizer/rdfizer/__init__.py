7fcf5ce8676e191742e81af57625c4cbf7616647
Daniel Doña <daniel.dona@xnor.ga>
2020-10-26 00:37:02 +0100

    Some rework

--- rdfizer/rdfizer/__init__.py
+++ rdfizer/rdfizer/__init__.py
@@ -4011,49 +4011,6 @@ def semantify(config_path):
 
 		
 
-def main():
-
-	"""
-	Function executed when the current file is executed as a script, instead of being
-	executed as a Python package in another script.
-
-	When executing the current file as a script in the terminal, the following flags
-	are accepted:
-
-	-h (python3 semantify.py -h): prompts the correct use of semantify.py as a script
-	-c (python3 semantify.py -c <config_file>): executes the program as a script with
-		with the <config_file> parameter as the path to the configuration file to be
-		used
-	--config_file (python3 semantify.py --config_file <config_file>): same behaviour
-		as -c flag
-
-	Parameters
-	----------
-	Nothing
-
-	Returns
-	-------
-	Nothing
-
-	"""
-
-	argv = sys.argv[1:]
-	try:
-		opts, args = getopt.getopt(argv, 'hc:', 'config_file=')
-	except getopt.GetoptError:
-		print('python3 semantify.py -c <config_file>')
-		sys.exit(1)
-	for opt, arg in opts:
-		if opt == '-h':
-			print('python3 semantify.py -c <config_file>')
-			sys.exit()
-		elif opt == '-c' or opt == '--config_file':
-			config_path = arg
-
-	semantify(config_path)
-
-if __name__ == "__main__":
-	main()
 
 """
 According to the meeting held on 11.04.2018, semantifying json files is not a top priority right
