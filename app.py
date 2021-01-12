5354095c8f550e7fb1968f673305e87baacaf1f3
Enrique <eiglesias34@gmail.com>
2021-01-12 12:40:08 +0100

    Corrected message in api

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/graph_creation/<path:config_file>', methods=['GET','POST'])
 def rdfgraph(config_file):
 	os.system("python3 /app/rdfizer/run_rdfizer.py /" + config_file)
-	return "The file has been semantified  " + str(config_file)
+	return "The file has been semantified " + str(config_file) + "\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4000)
