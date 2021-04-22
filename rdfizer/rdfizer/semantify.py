18c7413b74d91db5adef19501cb2ae4277b7cee9
Kemele M. Endris <keme686@gmail.com>
2021-04-22 00:06:20 -0700

    fix sematify-mysql 'graph' var reference
    "UnboundLocalError: local variable 'graph' referenced before assignment" error

--- rdfizer/rdfizer/semantify.py
+++ rdfizer/rdfizer/semantify.py
@@ -3015,7 +3015,7 @@ def semantify_mysql(row, row_headers, triples_map, triples_map_list, output_file
 		for rdf_class in triples_map.subject_map.rdf_class:
 			if rdf_class is not None:
 				obj = "<{}>".format(rdf_class)
-				for graph in graph:
+				for graph in  triples_map.subject_map.graph:
 					rdf_type = subject + " " + predicate + " " + obj +" .\n"
 					if graph is not None and "defaultGraph" not in graph:
 						if "{" in triples_map.subject_map.graph:	
